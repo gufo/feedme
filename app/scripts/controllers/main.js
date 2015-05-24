7b84ef0703ca09ac009686c7885ebe2b982212ec
Janko Luin <janko@luin.se>
2015-05-24 09:30:15 +0200

    RED: Fetch data from a remote source
    The controller unit test is now green, but the integration test is red:
    our application is trying to fetch data from a location that is not
    being served by the test framework. We need to provide mock data there,
    too.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,8 @@
  * Controller of the feedmeApp
  */
 angular.module('feedmeApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.venue = {name: 'Loungen'};
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/places/nearby.json').success(function(data) {
+      $scope.venue = data.response.groups[0].items[0].venue;
+    });
   });
