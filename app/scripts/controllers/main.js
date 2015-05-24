aa1ef66db17704a6c0eb22d66870c61524b5f592
Janko Luin <janko@luin.se>
2015-05-24 09:43:48 +0200

    ERRATIC: Expose a randomly selected venue
    This change will cause the integration tests to be green half of the
    time, red the other half. After all, they are still expecting a single
    hard-coded value, where the application is now selecting at random
    between our two mocks: "Loungen" and "Matlåda i köket".

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,8 @@
 angular.module('feedmeApp')
   .controller('MainCtrl', function ($scope, $http) {
     $http.get('/places/nearby.json').success(function(data) {
-      $scope.venue = data.response.groups[0].items[0].venue;
+      var items = data.response.groups[0].items;
+      var itemId = Math.floor(Math.random() * items.length);
+      $scope.venue = items[itemId].venue;
     });
   });
