6fc2bd66dba0368d11a54ed3adad86c996196370
Janko Luin <janko@luin.se>
2015-05-24 09:49:59 +0200

    GREEN: Allow for random selection in the integration test
    This is a dead-simple solution which probably won't hold water for long,
    but it's enough to support our simple demo app.
    
    Notice how the expectation suddenly becomes asynchronous. We're dealing
    with a leaky abstraction: all calls to the browser are done
    asynchronously, but the `expect()` wrapper hides it by knowing about
    promises and resolving them. Assertions like `.toContain()`, however, do
    not have the same magic properties, so we must handle it ourselves.

--- protractor/spec/example_spec.js
+++ protractor/spec/example_spec.js
@@ -4,7 +4,8 @@ describe('FEEDME application', function() {
   it('should suggest a nearby lunch venue', function() {
     browser.get('/');
 
-    var heading = element(by.css('h1'));
-    expect(heading.getText()).toEqual('Loungen');
+    element(by.css('h1')).getText().then(function(actual) {
+      expect(['Loungen', 'Matlåda i köket']).toContain(actual);
+    });
   });
 });
